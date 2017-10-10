44b12c65ec604524e6895946b840510001e24817
Luz <luz.skn@gmail.com>
2017-10-10 17:11:17 -0500

    añadiendo mi segundo cambio

--- js/app.js
+++ js/app.js
@@ -8,16 +8,16 @@ for(i=0;i<10;i++)
    var number = parseInt(divididoArray[i]);
    if(i%2!==0)
     {
-      var pairMultiplied=divididoArray[i]*2;
-       if(pairMultiplied>=10)
-        {
-         firstValue=parseInt(pairMultiplied/10)//1,2--->1
-         secondValue=pairMultiplied%10; //12%10--->2
-         sumValues=firstValue+secondValue;//1+2=3
-         divididoArray[i]=sumValues;//divididoArray[2]=3
-         }else{
-           divididoArray[i]=pairMultiplied;
-         }
+     var pairMultiplied=divididoArray[i]*2;
+    if(pairMultiplied>=10)
+    {
+    firstValue=parseInt(pairMultiplied/10)//1,2--->1
+    secondValue=pairMultiplied%10; //12%10--->2
+    sumValues=firstValue+secondValue;//1+2=3
+    divididoArray[i]=sumValues;//divididoArray[2]=3
+    }else{
+          divididoArray[i]=pairMultiplied;
+      }
 
      }else{
        divididoArray[i]=parseInt(divididoArray[i]);

5de7eb506ef9cf590b194ef03b655118c227ac32
Luz <luz.skn@gmail.com>
2017-10-10 17:52:00 -0500

    añadiendo ultimos detalles

--- js/app.js
+++ js/app.js
@@ -5,31 +5,29 @@ var divididoArray=numeroTarjeta.split("");//divide el strin enviando y lo convie
 divididoArray.reverse();//revierte la posición de los elementos del Array
 var suma=0;
 for(i=0;i<10;i++)//haciendo iteración del Array--->divididoArray
- {
+{
    var number = parseInt(divididoArray[i]);//convierte el string recibido a número y lo guarda en una variable
    if(i%2!==0)
     {
-     var pairMultiplied=divididoArray[i]*2; //pairMultiplied ---> guarda la multiplicación del elemento que cumple la condición * 2
-    if(pairMultiplied>=10)
-    {
-    var firstValue=parseInt(pairMultiplied/10)//firstValue---> guarda el primer dígito que resultó de la multiplicación
-    var secondValue=pairMultiplied%10; //secondValue---> guarda el segundo dígito del número que resultó de la multiplicacion.
-    var sumValues=firstValue+secondValue;//sumValues---> suma ambos digitos.
+    var pairMultiplied=divididoArray[i]*2; //pairMultiplied ---> guarda la multiplicación del elemento que cumple la condición * 2
+     if(pairMultiplied>=10)
+     {
+      var firstValue=parseInt(pairMultiplied/10)//firstValue---> guarda el primer dígito que resultó de la multiplicación
+      var secondValue=pairMultiplied%10; //secondValue---> guarda el segundo dígito del número que resultó de la multiplicacion.
+      var sumValues=firstValue+secondValue;//sumValues---> suma ambos digitos.
         divididoArray[i]=sumValues;//el elemento del array en dicha posición tomará como valor la suma de ambos dígitos.
         suma=suma+divididoArray[i];
-     }
-    }else{
+      }else{
           divididoArray[i]=pairMultiplied;
           suma=suma+divididoArray[i];
-     }
-      }
+         }
+   }else
+    {
+    divididoArray[i]=parseInt(divididoArray[i]);
+    suma=suma+divididoArray[i];
+    }
+}
 
-     }else{
-       divididoArray[i]=parseInt(divididoArray[i]);
-       suma=suma+divididoArray[i];
-     }
-     }
- }
  //verificando si la tarjeta es valida o inválida
  if(suma%10===0){
    return "tarjeta valida"

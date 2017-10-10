0afadc49dae50371c716cbc97abbf70fd53f0d65
Luz <luz.skn@gmail.com>
2017-10-10 17:42:41 -0500

    añadiendo datos al usuario

--- js/app.js
+++ js/app.js
@@ -1,12 +1,10 @@
 //validar número de tarjeta usando el algoritmo luhn
 function validar(numeroTarjeta)
 {
-//divide el strin enviando y lo convierte en un Array
-divididoArray=numeroTarjeta.split("");
-//revierte la posición de los elementos del Array
-divididoArray.reverse();
-//haciendo iteración del Array--->divididoArray
-for(i=0;i<10;i++)
+var divididoArray=numeroTarjeta.split("");//divide el strin enviando y lo convierte en un Array
+divididoArray.reverse();//revierte la posición de los elementos del Array
+var suma=0;
+for(i=0;i<10;i++)//haciendo iteración del Array--->divididoArray
  {
    var number = parseInt(divididoArray[i]);//convierte el string recibido a número y lo guarda en una variable
    if(i%2!==0)
@@ -14,19 +12,29 @@ for(i=0;i<10;i++)
      var pairMultiplied=divididoArray[i]*2; //pairMultiplied ---> guarda la multiplicación del elemento que cumple la condición * 2
     if(pairMultiplied>=10)
     {
-    firstValue=parseInt(pairMultiplied/10)//firstValue---> guarda el primer dígito que resultó de la multiplicación
-    secondValue=pairMultiplied%10; //secondValue---> guarda el segundo dígito del número que resultó de la multiplicacion.
-    sumValues=firstValue+secondValue;//sumValues---> suma ambos digitos.
-    divididoArray[i]=sumValues;//el elemento del array en dicha posición tomará como valor la suma de ambos dígitos.
+    var firstValue=parseInt(pairMultiplied/10)//firstValue---> guarda el primer dígito que resultó de la multiplicación
+    var secondValue=pairMultiplied%10; //secondValue---> guarda el segundo dígito del número que resultó de la multiplicacion.
+    var sumValues=firstValue+secondValue;//sumValues---> suma ambos digitos.
+        divididoArray[i]=sumValues;//el elemento del array en dicha posición tomará como valor la suma de ambos dígitos.
+        suma=suma+divididoArray[i];
+     }
     }else{
           divididoArray[i]=pairMultiplied;
+          suma=suma+divididoArray[i];
+     }
       }
 
      }else{
        divididoArray[i]=parseInt(divididoArray[i]);
+       suma=suma+divididoArray[i];
+     }
      }
  }
-return divididoArray;
+ //verificando si la tarjeta es valida o inválida
+ if(suma%10===0){
+   return "tarjeta valida"
+  }
+ return "tarjeta invalida"
 }
 //preguntando datos al usuario.
 do{

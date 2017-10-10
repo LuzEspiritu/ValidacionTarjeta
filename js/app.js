76be839672fed95e31b845464cbab9ef724340d2
Luz <luz.skn@gmail.com>
2017-10-10 18:07:07 -0500

    detalles

--- js/app.js
+++ js/app.js
@@ -6,18 +6,18 @@ divididoArray.reverse();//revierte la posición de los elementos del Array
 var suma=0;
 for(i=0;i<10;i++)//haciendo iteración del Array--->divididoArray
 {
-   var number = parseInt(divididoArray[i]);//convierte el string recibido a número y lo guarda en una variable
-   if(i%2!==0)
+  var number = parseInt(divididoArray[i]);//convierte el string recibido a número y lo guarda en una variable
+  if(i%2!==0)
+  {
+  var pairMultiplied=divididoArray[i]*2; //pairMultiplied ---> guarda la multiplicación del elemento que cumple la condición * 2
+   if(pairMultiplied>=10)
     {
-    var pairMultiplied=divididoArray[i]*2; //pairMultiplied ---> guarda la multiplicación del elemento que cumple la condición * 2
-     if(pairMultiplied>=10)
-     {
-      var firstValue=parseInt(pairMultiplied/10)//firstValue---> guarda el primer dígito que resultó de la multiplicación
-      var secondValue=pairMultiplied%10; //secondValue---> guarda el segundo dígito del número que resultó de la multiplicacion.
-      var sumValues=firstValue+secondValue;//sumValues---> suma ambos digitos.
-        divididoArray[i]=sumValues;//el elemento del array en dicha posición tomará como valor la suma de ambos dígitos.
-        suma=suma+divididoArray[i];
-      }else{
+    var firstValue=parseInt(pairMultiplied/10)//firstValue---> guarda el primer dígito que resultó de la multiplicación
+    var secondValue=pairMultiplied%10; //secondValue---> guarda el segundo dígito del número que resultó de la multiplicacion.
+    var sumValues=firstValue+secondValue;//sumValues---> suma ambos digitos.
+    divididoArray[i]=sumValues;//el elemento del array en dicha posición tomará como valor la suma de ambos dígitos.
+    suma=suma+divididoArray[i];
+    }else{
           divididoArray[i]=pairMultiplied;
           suma=suma+divididoArray[i];
          }
